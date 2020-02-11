4038a239ee3b52a61b453caf16d06eee858d54b6
Matthew Hutchings <matthew@fewzen.com>
2020-02-11 12:36:26 +0000

    correction

--- src/CleanupRules.php
+++ src/CleanupRules.php
@@ -15,7 +15,7 @@ class CleanupRules
             'anahkiasen/html-object'                => array($docs, 'phpunit.xml* tests/*'),
             'anahkiasen/rocketeer'                  => array($docs, $tests),
             'anahkiasen/underscore-php'             => array($docs, $tests),
-            'aws/aws-sdk-php'                 		=> array($docs, $tests),
+            'aws/aws-sdk-php'                 		=> array($docs, $tests, 'src/XSD2PHP'),
             'barryvdh/composer-cleanup-plugin'      => array($docs, $tests),
             'barryvdh/laravel-debugbar'             => array($docs, $tests),
             'barryvdh/laravel-ide-helper'           => array($docs, $tests),
